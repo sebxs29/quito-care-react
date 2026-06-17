db9b8cc3306b3030c2d8473ea3874226de2f511b
Sebastian-Caiza <darwin.caiza@epn.edu.ec>
2026-06-16 20:55:03 -0500

    agregar animacion AOS en columnas del footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,39 +1,38 @@
-
 import "./Footer.css"
+import { useEffect } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
 
 const Footer = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    })
+  }, [])
+
   return (
     <footer className="footer">
       <div className="footer__container container">
 
-        <div className="footer__brand">
+        <div className="footer__brand" data-aos="fade-up" data-aos-delay="0">
           <h3 className="footer__logo">
             Quito
             <span className="footer__logo--highlight">Care</span>
           </h3>
-
           <p className="footer__description">
             Telemedicina en Quito rápida, fácil y segura
           </p>
-
           <div className="footer__social">
-            <a href="#">
-              <i className="fa-brands fa-facebook"></i>
-            </a>
-
-            <a href="#">
-              <i className="fa-brands fa-instagram"></i>
-            </a>
-
-            <a href="#">
-              <i className="fa-brands fa-youtube"></i>
-            </a>
+            <a href="#"><i className="fa-brands fa-facebook"></i></a>
+            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+            <a href="#"><i className="fa-brands fa-youtube"></i></a>
           </div>
         </div>
 
-        <div className="footer__links">
+        <div className="footer__links" data-aos="fade-up" data-aos-delay="100">
           <h4 className="footer__title">Enlaces</h4>
-
           <ul>
             <li><a href="#">Inicio</a></li>
             <li><a href="#">Servicios</a></li>
@@ -43,9 +42,8 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="footer__services">
+        <div className="footer__services" data-aos="fade-up" data-aos-delay="200">
           <h4 className="footer__title">Servicios</h4>
-
           <ul>
             <li><a href="#">Consulta en línea</a></li>
             <li><a href="#">Especialidades</a></li>
@@ -54,9 +52,8 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="footer__contact">
+        <div className="footer__contact" data-aos="fade-up" data-aos-delay="300">
           <h4 className="footer__title">Contacto</h4>
-
           <p>Quito, Ecuador</p>
           <p>info@quitocare.com</p>
           <p>+593 98 496 9316</p>
@@ -68,7 +65,7 @@ const Footer = () => {
         <p>© 2026 QuitoCare. Todos los derechos reservados.</p>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
