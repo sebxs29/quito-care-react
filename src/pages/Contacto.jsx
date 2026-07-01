56402d0fe255b003a99eaf61ac942043ce89022c
Sebastian-Caiza <darwin.caiza@epn.edu.ec>
2026-06-30 19:27:44 -0500

    Add files via upload

---
+++ src/pages/Contacto.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header"
+import Contact from "../components/contact/Contact"
+import Footer from "../components/footer/Footer"
+
+const Contacto = () => {
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default Contacto
