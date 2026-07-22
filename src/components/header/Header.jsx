dd42f59ab5754e64cbee24d7b0c6edb801aa7534
sebxs29 <sebas29t@gmail.com>
2026-07-22 17:06:12 -0500

    agregar menú hamburguesa, header fijo y mejorar footer responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import './Header.css'
 import { Link } from 'react-router'
 import TypeIt from 'typeit-react'
 
 const Header = () => {
+// MENU HAMBURGESA
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  const cerrarMenu = () => {
+    setMenuAbierto(false);
+  }
+
   return (
     <header className="header">
       <nav className="nav__header container">
@@ -19,23 +27,29 @@ const Header = () => {
               Quito<span className="navbar__logo--primary">Care</span>
             </TypeIt>
           </h1>
+
+          <button
+            type='button'
+            className={`nav__hamburger ${menuAbierto ? "nav__hamburger--activo" : ""}`}
+              onClick={() => setMenuAbierto(!menuAbierto)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
         </div>
 
-        <ul className="nav__menu" id="navMenu">
-          <li><Link to="/">Inicio</Link></li>
-          <li><Link to="/servicios">Servicios</Link></li>
-          <li><Link to="/nosotros">Nosotros</Link></li>
-          <li><Link to="/contacto">Contacto</Link></li>
-          <li>
-            <Link to="/login" className="nav__login-btn">
-              LOGIN
-            </Link>
-          </li>
-          <li>
-            <Link to="/registro" className="nav__register-btn">
-              REGISTRO
-            </Link>
-          </li>
+        <ul
+          className={`nav__menu ${menuAbierto ? "nav__menu--activo" : ""}`}
+          id="navMenu"
+        >
+          <li><Link to="/" onClick={cerrarMenu}>Inicio</Link></li>
+          <li><Link to="/servicios" onClick={cerrarMenu}>Servicios</Link></li>
+          <li><Link to="/nosotros" onClick={cerrarMenu}>Nosotros</Link></li>
+          <li><Link to="/contacto" onClick={cerrarMenu}>Contacto</Link></li>
+          <li><Link to="/login" className="nav__login-btn" onClick={cerrarMenu}>LOGIN</Link></li>
+          <li><Link to="/registro" className="nav__register-btn" onClick={cerrarMenu}>REGISTRO</Link></li>
         </ul>
 
       </nav>
